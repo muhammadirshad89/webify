0e7aa7b97d855bf9ed46fb0aca360d8ddc68f37c
muhammad irshad <syyed.asshi89@gmail.com>
2020-08-13 11:46:39 +0500

    modified 7

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 function saveData(){
     var firstName = document.getElementById("firstName");
+    var email = document.getElementById("email");
+    var number = document.getElementById("number")
+    var message = document.getElementById("message");
+
+    var client = {
+        firstName: firstName.value,
+        email: email.value,
+        number: number.value,
+        message: message.value
+    }
+    console.log(client)
 }
